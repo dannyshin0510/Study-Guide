838f0b50b12d67be869de4cb87ce84b50ce27f0b
dannyshin0510 <42787400+dannyshin0510@users.noreply.github.com>
2021-01-09 16:17:02 -0500

    some more list ops

---
+++ lists/product_of_two.py
@@ -0,0 +1,19 @@
+# Given the array of integers nums, you will choose two different indices i and j of that array.
+# Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+
+# Thought: simple; need two largest vals in the list
+
+# Two solutions:
+
+#First: requiring traversal TWICE, due to 'max()' method used twice
+def maxProduct(self, nums: List[int]) -> int:
+    first = max(nums)
+    nums.remove(first)
+    second = max(nums)
+    return (first - 1) * (second - 1)
+
+#Second: requires traversing once, since we are using '.sort()'
+def maxProduct(self, nums: List[int]) -> int:
+    nums.sort()
+    return (nums[len(nums) - 1] - 1) * (nums[len(nums) - 2] - 1)
