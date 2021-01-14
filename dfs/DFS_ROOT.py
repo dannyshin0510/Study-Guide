12c891bef753b80fce4d3fe29fb981d086e384fa
dannyshin0510 <42787400+dannyshin0510@users.noreply.github.com>
2021-01-13 23:20:43 -0500

    root algos added

---
+++ dfs/DFS_ROOT.py
@@ -0,0 +1,20 @@
+# The following is the ROOT DFS algorithm.
+
+# Using a Python dictionary to act as an adjacency list
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+visited = set() # Set to keep track of visited nodes.
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
